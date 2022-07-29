d5c4c429f7b746435192c9709b45c38295649843
dannylin <a752774@gmail.com>
2022-07-29 15:31:04 +0800

    fix

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -1,58 +1,62 @@
-import unittest
 
-class linked_list_node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 
 class LinkedList:
     def __init__(self):
-        self.head=None
-        self.tail=None
+        self.head = None
+        self.tail = None
 
-    def append(self,item):
-        if not isinstance(item,linked_list_node):
-            item=linked_list_node(item)
-        if self.head==None:
-            self.head=item
+    def append(self, item):
+        if not isinstance(item, LinkedListNode):
+            item = LinkedListNode(item)
+        if self.head is None:
+            self.head = item
         else:
-            self.tail.next=item
-        self.tail=item
+            self.tail.next = item
+        self.tail = item
 
     def length(self):
         current = self.head
-        length=0
-        while current != None:
-            length+=1
+        length = 0
+        while current is not None:
+            length += 1
             current = current.next
         return length
 
     def output_list(self):
-        current=self.head
-        while current!=None:
+        current = self.head
+        while current is not None:
             print(current.data)
-            current=current.next
+            current = current.next
 
-    def index(self,goal):
+    def index(self, goal):
         current = self.head
-        count=0
-        while current!=None:
-            if current.data==goal:
-                return(count)
-            else:
-                count+=1
-                current=current.next
-    def remove(self,index):
-        current = self.head
-        previous=None
         count = 0
-        while current!=None:
-            if count==index:
-                previous.next=current.next
+
+        while current is not None:
+            if current.data == goal:
+                return count
+            else:
+                count += 1
+                current = current.next
+
+    def remove(self, index):
+        current = self.head
+        previous = None
+        count = 0
+        while current is not None:
+            if count == index:
+                previous.next = current.next
                 break
             else:
                 count += 1
-                previous=current
-                current=current.next
+                previous = current
+                current = current.next
 
-list=LinkedList()
+
+List = LinkedList()
--- SingleLinkedListTest.py
+++ SingleLinkedListTest.py
@@ -1,28 +1,30 @@
 import random
-
 from SingleLinkedList import LinkedList
 import unittest
 
 
 class LinkedListTest(unittest.TestCase):
-    def test_append(self):
-        list = LinkedList()
-        list.append(random.randint(0,100))
-        self.assertTrue(list.head.data)
-    def test_length(self):
-        list=LinkedList()
-        for i in range(10):
-            list.append(i)
-        self.assertEqual(list.length(),10)
-    def test_index(self):
-        list=LinkedList()
-        for i in range(10):
-            list.append(i)
-        self.assertEqual(list.index(4),4)
-    def test_remove(self):
-        list=LinkedList()
-        for i in range(10):
-            list.append(i)
-        list.remove(2)
-        self.assertEqual(list.index(3),2)
 
+    def test_append(self):
+        linkedlist = LinkedList()
+        linkedlist.append(random.randint(0, 100))
+        self.assertTrue(linkedlist.head.data)
+
+    def test_length(self):
+        linkedlist = LinkedList()
+        for i in range(10):
+            linkedlist.append(i)
+        self.assertEqual(linkedlist.length(), 10)
+
+    def test_index(self):
+        linkedlist = LinkedList()
+        for i in range(10):
+            linkedlist.append(i)
+        self.assertEqual(linkedlist.index(4), 4)
+
+    def test_remove(self):
+        linkedlist = LinkedList()
+        for i in range(10):
+            linkedlist.append(i)
+        linkedlist.remove(2)
+        self.assertEqual(linkedlist.index(3), 2)
