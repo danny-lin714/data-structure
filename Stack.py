06d8e51535763081fa35444187ff67e242cff9ae
dannylin <a752774@gmail.com>
2022-07-29 16:27:56 +0800

    fix

--- Stack.py
+++ Stack.py
@@ -1,5 +1,4 @@
 """
-
 implement:stack
 operation:
     push
@@ -11,17 +10,25 @@ operation:
 
 
 class Stack:
+
     def __init__(self):
         self.element = []
 
-    def push(self, data):
+    def push(self, data) -> None:
         self.element.append(data)
 
-    def pop(self):
+    def pop(self) -> None:
         if len(self.element) != 0:
             self.element.pop()
-        else:
-            None
+
+    def size(self) -> int:
+        return len(self.element)
+
+    def peek(self) -> (int, str, bool, float, list, set, dict, tuple):
+        return self.element[-1]
+
+    def empty(self) -> bool:
+        return not bool(self.element)
+
 
 stack = Stack()
-print(stack.element)
